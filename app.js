c1c48623558f3c96d8cddc8ede2dd63798c15893
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-08-17 16:13:36 -0400

    Added materialize and jQuery links. Configured mongoose and method-override.

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Connect to the Database
+mongoose.connect('mongodb://localhost/express-movies');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +26,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(methodOverride('_method'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
