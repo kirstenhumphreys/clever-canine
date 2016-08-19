485b57f3e529969556c0f6284bac212ccc9fd328
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-08-18 23:28:04 -0400

    Added some more routes

--- routes/items.js
+++ routes/items.js
@@ -8,24 +8,60 @@ function makeError(res, message, status) {
   return error;
 }
 
-let items = [
-  {
-    title: 'Small Dog',
-    plan: '1 month'
-  },
-  {
-    title: 'Medium Dog',
-    plan: '3 month'
-  },
-  {
-    title: 'Large Dog',
-    plan: '6 month'
-  }
-];
+// let items = [
+//   {
+//     title: 'Small Dog',
+//     plan: '1 month'
+//   },
+//   {
+//     title: 'Medium Dog',
+//     plan: '3 month'
+//   },
+//   {
+//     title: 'Large Dog',
+//     plan: '6 month'
+//   }
+// ];
 
-//Index
+//INDEX
 router.get('/', function(req, res, next) {
-  res.render('items/index', { items: items});
+  Item.find({})
+  .then(function(todos) {
+    res.render('items/index', { item: item });
+  }, function(err) {
+    return next(err);
+  });
 });
+
+//NEW
+router.get('/new', function(req, res, next) {
+  var item = {
+    title: '',
+    plan: '',
+    inCart: false
+  };
+  res.render('items/new', { item: item } );
+});
+
+//SHOW
+
+router.get('/:id', function(req, res, next) {
+  item.findbyId(req.params.id)
+  .then(function(todo) {
+    if (!item) return next(makeError(res, 'Document not found', 404));
+    res.render('items/show', { item: item });
+  }, function(err) {
+    return next(err);
+  });
+});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
