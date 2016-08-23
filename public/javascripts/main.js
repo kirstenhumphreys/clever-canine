f070dba4d3500d9f31e040b5461cdfaf486b7089
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-08-23 16:41:57 -0400

    fixed bug with parallax scroll

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,35 @@
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
+// (function($){
+//   $(function(){
+
+//     $('.button-collapse').sideNav();
+
+//   });
+
+//   $(document).ready(function() {
+//     $('select').material_select();
+//   });
+
+//   $(document).ready(function(){
+//       $('.parallax').parallax();
+//   });
+//   // end of document ready
+// })(jQuery); // end of jQuery name space
+
 
 $(document).ready(function() {
- $('select').material_select();
+  var temp = $('select')
+
+  if (temp) {
+    temp.material_select();
+  }
 });
 
+$(document).ready(function(){
+  var parallaxTemp = $('.parallax')
+
+    if (parallaxTemp) {
+      parallaxTemp.parallax();
+    }
+
+    });
 
